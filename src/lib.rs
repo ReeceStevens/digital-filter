8192289a446be7be24299c0833ebbbdd6a033c4a
Reece Stevens <reecestevens24@gmail.com>
2018-06-23 11:52:43 -0500

    Switch to using generic-array and add basic test

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,59 @@
 //! # Digital Filter
 //!
-//! This crate will provide an interface to a digital FIR filter implementation. By passing in the
-//! filter weights and a matching empty buffer, this crate will instantiate a digital filter that
-//! will accept a vector or stream of inputs and produce a vector or stream of filtered
-//! outputs.
+//! This crate will provide an interface to a digital FIR filter implementation for no-std
+//! environments that cannot depend on a heap being present. By passing in the filter weights and a
+//! matching empty buffer, this crate will instantiate a digital filter that will accept a stream
+//! of inputs and produce a stream of filtered outputs.
 //!
-//! This crate will *not* try to impose any higher-level meaning on the output of the filter. This
-//! means that issues such as filter delay will not be addressed.
-//!
-//! The primary objective of this crate is to abstract away the process of moving data through a
-//! pre-defined digital filter. It doesn't perform filter design, so you'll need an external tool
-//! to design the filter weights for input. Matlab has an excellent tool for this, and there are
-//! several open-source alternatives as well. A list of possible tools will be added here for
-//! reference later.
+//! This crate is effectively an implementation of the `lfilter` function in SciPy. The goal of
+//! this crate is to be a self-contained way to apply a digital filter in an embedded system. It
+//! doesn't perform filter design, so you'll need an external tool to design the filter weights for
+//! input. SciPy and Matlab both have excellent tools for this (`scipy.signal.firwin` for SciPy).
+
 #![no_std]
 
 use core::ops::IndexMut;
 
+#[macro_use]
+extern crate generic_array;
 extern crate heapless;
+extern crate typenum;
+
+use core::ops::Add;
 use heapless::RingBuffer;
 use heapless::consts::*;
+use generic_array::{GenericArray, ArrayLength};
+use typenum::Sum;
 
-type FilterBuf = [f32; 3];
-type FilterRing = RingBuffer<f32, U3>;
+type FilterItem = f32;
+type FilterBuf<N: ArrayLength<FilterItem>> = GenericArray<FilterItem,N>;
+type FilterRing<N: ArrayLength<FilterItem>> = RingBuffer<FilterItem, N>;
 
-struct DigitalFilter {
-    coeffs: FilterBuf,
-    buffer: FilterRing,
+struct DigitalFilter<N>
+where
+    N: Add<U1> + ArrayLength<FilterItem>,
+    Sum<N, U1>: ArrayLength<FilterItem>
+{
+    coeffs: FilterBuf<N>,
+    buffer: FilterRing<N>,
     num_taps: usize
 }
 
-impl DigitalFilter {
-    fn new(coeffs: FilterBuf, buffer: FilterRing) -> Self {
+impl<N> DigitalFilter<N>
+where
+    N: Add<U1> + ArrayLength<FilterItem>,
+    Sum<N, U1>: ArrayLength<FilterItem>
+{
+    fn new(coeffs: FilterBuf<N>, mut buffer: FilterRing<N>) -> Self {
         let num_taps = coeffs.len();
+        // Initialize the buffer
+        for idx in 0..num_taps {
+            buffer.enqueue(0.).unwrap();
+        }
         DigitalFilter { coeffs, buffer, num_taps }
     }
 
-    fn filter(mut self, input: f32) -> f32 {
+    fn filter(&mut self, input: f32) -> f32 {
         let _ = self.buffer.dequeue();
         self.buffer.enqueue(input).unwrap();
         let mut output: f32 = 0_f32;
@@ -53,33 +70,17 @@ mod tests {
     #[test]
     fn basic_filter_test() {
         use DigitalFilter;
-        let mut coeffs = [1, 1, 1];
-        let mut buffer = [0, 0, 0];
-        let filter = DigitalFilter::new(coeffs, buffer);
-        let inputs = [4, 8, 15, 16, 23, 42];
-        let expected_output = [0, 0, 0, 4, 8, 15];
-        let actual_output = inputs.iter().map(|item| filter.filter(item)).collect();
+        use heapless::RingBuffer;
+        use heapless::consts::{U3,U6};
+        let mut coeffs = arr![f32; 1., 1., 1.];
+        let mut buffer: RingBuffer<f32,U3> = RingBuffer::new();
+        let mut filter = DigitalFilter::new(coeffs, buffer);
+        let inputs = [4., 8., 15., 16., 23., 42.];
+        let expected_output = [4., 12., 27., 39., 54., 81.];
+        let mut actual_output = [0.; 6];
+        for (idx, input) in inputs.iter().enumerate() {
+            actual_output[idx] = filter.filter(*input);
+        }
         assert_eq!(expected_output, actual_output);
     }
-
-    // #[test]
-    // fn sos_filter_test() {
-    //     let sos_filter: [[f32; 6]; 5] = [
-    //         [1., 2., 1., 1., -1.4818, 0.8316],
-    //         [1., 2., 1., 1., -1.2772, 0.5787],
-    //         [1., 2., 1., 1., -1.1430, 0.4128],
-    //         [1., 2., 1., 1., -1.0619, 0.3126],
-    //         [1., 2., 1., 1., -1.0237, 0.2654]
-    //     ];
-    //     let gain: [f32; 5] = [0.0875, 0.754, 0.0675, 0.0627, 0.0604];
-    //     // let filter = DigitalFilter(sos_filter, gain);
-
-    //     let input: [i32; 7] = [0,-1,0,1,0,-1,0];
-    //     // let output = input.into_iter().map(|i| filter.process(i));
-    //     let mut output = [0; 7];
-    //     for (idx, o) in input.into_iter().map(|i| -1 * i).enumerate() {
-    //         output[idx] = o;
-    //     }
-    //     assert_eq!(output, [0,1,0,-1,0,1,0]);
-    // }
 }
